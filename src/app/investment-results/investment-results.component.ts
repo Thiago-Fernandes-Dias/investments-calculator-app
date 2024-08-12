945dfa50d4074e25473353d88289036798df45a4
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2024-08-11 22:20:50 -0300

    feat: Finish investment results component first look.

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -3,8 +3,6 @@ import { InvestmentResultsService, InvestmentResults } from '../investments-resu
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
